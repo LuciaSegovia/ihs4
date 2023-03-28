525deb01dbeef0de1eb7f626e7e0185c51f77637
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-03-28 15:28:01 +0100

    cleaning own produced variable

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Micronutrient Analysis of the Fourth Integrated Household Survey of Malawi
 
 
-Analysis of differences between nutrient density and adult-male equivalent metrics using Malawi's Fourth Integrated Household Survey (IHS4)
-<<<<<<< HEAD
- 
-=======
+Analysis of differences between nutrient density and adult-male equivalent metrics using Malawi's Fourth Integrated Household Survey (IHS4) (https://doi.org/10.48529/g2p9-9r19)
 
 
-The raw data file can be downloaded from https://microdata.worldbank.org/index.php/catalog/2936
+The raw data file can be downloaded from https://microdata.worldbank.org/index.php/catalog/2936. The data files needed to run this analysis are:
 
->>>>>>> f0ebf8bc1d001a6f116302fbdeb7f98577ecfbcd
+1. HH_MOD_G1
+2. HH_MOD_A_FILT 
+
+
+Please, for the scripts to run you need to:
+
+1. Download the raw data 
+2. Create a folder in your local version of this project called `data`
+3. Unzip raw data and paste the above mentioned files inside the `data\` (folder)
+4. Create a folder called `inter-output` to store processed files
+
+Happy coding :)
+
+
--- ihs4_cleaning.R
+++ ihs4_cleaning.R
@@ -6,7 +6,15 @@ library(tidyverse)
 #-------Read in the file------#
 #
 
-#ihs4 <- read.csv(here::here('data', 'hces', 'mwi','ihs4', 'hh_mod_g1.csv')
+ihs4 <- read.csv(here::here("data", "inter-output", "hh_mod_g_processed.csv"))
+wb_factor <- read.csv(here::here('data', "factors", 'wb_conv_factors_mwi.csv'), fileEncoding="UTF-8-BOM")
+nsu.df <- read.csv(here::here('data', "factors", 'nsu.factors.csv'), fileEncoding="UTF-8-BOM")
+
+# 
+
+nsu.df <- nsu.df %>% 
+  mutate(cons_unitA = str_extract(measure_id, "(?<=\\_)[:alnum:]+")) %>% 
+  left_join(., ihs4 %>% distinct(cons_unitA, unit))
 
 #Rename variables
 
@@ -63,4 +71,152 @@ HHitem_code <-  HHitem_code %>%
                                                                      ifelse(code %in% 801:804, 'Sugar, Fats, and Oil', 
                                                                             ifelse(code %in% 901:916, 'Beverages',
                                                                                    ifelse(code %in% 810:818, 'Spices & Miscellaneous', 'Meat, Fish and Animal products')))))))))))
+
+
+# Cleaning "own production" NSU
+
+ihs4 %>% distinct(prod_unitA)
+
+#Standardisation of NA values
+ihs4$prod_unitA[!grepl("\\d", ihs4$prod_unitA)] <- NA
+ihs4$prod_unit_oth[grepl("N\\/A", ihs4$prod_unit_oth)] <- NA
+
+ihs4 %>% left_join(., wb_factor, by = c("item_code", "prod_unitA" = "unit_code")) %>% 
+  filter(!is.na(prod_unitA) & is.na(wb_factor_n))
+
+wb_factor %>% filter(item_code == "25")
+wb_factor %>% filter(unit == "PAIL")
+
+nsu.df %>% filter(grepl("DOZEN", unit, ignore.case = TRUE)) %>% 
+  distinct(cons_unitA, unit)
+
+ihs4 %>% filter(prod_unitA==26)
+ihs4 %>% filter(grepl("DOZEN", prod_unit_oth, ignore.case = TRUE))
+ihs4 %>% filter(grepl("DOZEN", unit, ignore.case = TRUE))
+
+units_na <- ihs4 %>% 
+  mutate(prod_nsu = ifelse(prod_unitA!=23, prod_unitA, 
+        case_when(
+prod_unit_oth == "KILOGRAM" ~ "1",
+prod_unit_oth == "50 KG" ~ "2",
+prod_unit_oth %in% c("PAIL (LARGE)") ~ "4C",
+prod_unit_oth %in% c("PAILMEDIUM", "MEDIUMPALL") ~ "4B",
+prod_unit_oth %in% c("PAIL (SMALL)", "PAILSMALL") ~ "4A",
+prod_unit_oth %in% c("PAIL", "20L PAIL" ,"10LITREPAIL,"  ) ~ "4",
+prod_unit_oth == "PAIL (MEDIUM)" ~ "4B",
+prod_unit_oth %in% c("NO. 10 PLATE", "PLATE") ~ "6", 
+prod_unit_oth == "NO. 12 PLATE" ~ "7", 
+prod_unit_oth == "NO. 24 PLATE" ~ "7B", 
+prod_unit_oth %in% c("BIGBUNCH") ~ "8C",
+prod_unit_oth %in% c("BUNDLE", "BUCH", "BUSH") ~ "8",
+prod_unit_oth == "PIECE (SMALL)"~"9A", 
+prod_unit_oth %in% c("PIECE MEDIUM","PIECE (MEDIUM)") ~"9B",
+prod_unit_oth == "PIECE (LARGE)" ~ "9C", 
+prod_unit_oth %in% c("PIECES","PIECE", "MANGO", "PIECE (ONE)", "PEACE", "PIACE", "PEICE", "PIEC3", "MICE" ) ~ "9",
+prod_unit_oth %in% c("HEAP", "HEA", "HEP") ~ "10", 
+prod_unit_oth %in% c("HEAP", "HEAP (SMALL)","HEAP (MEDIUM)") ~ "10", 
+prod_unit_oth == "HEAP (LARGE)" ~ "10C", 
+prod_unit_oth == "BOTTLE" & item_code == "803"~ "15",
+prod_unit_oth == "CUP" ~ "16",
+prod_unit_oth == "GRAMS"~ "18", 
+prod_unit_oth %in% c("TEASPOON", "SPOON") ~ "20",
+prod_unit_oth %in% c("SATCHET (MEDIUM)") ~ "22B",
+prod_unit_oth %in% c("SATCHET (SMALL)") ~ "22B",
+prod_unit_oth %in% c("TUBE /SACHET","SATCHET (500 GRAM)", "TUBE") ~ "22",
+prod_unit_oth %in% c("TINA(FLAT)", "TINA(SMALL)" ) ~ "25A",
+prod_unit_oth %in% c("TINA (HEAPED)", "MEDIUM (PLATE)") ~ "25B",
+prod_unit_oth %in% c("5L BUCKET", "5 LITRES", "BUCKET") ~ "26", 
+prod_unit_oth %in% c("PACKET 100 GRAM", "PACKET 150 GRAM") ~ "34",
+prod_unit_oth == "SATCHET (20 GRAM)"~ "41",
+prod_unit_oth %in% c("SATCHET (MEDIUM)", "TUBE (MEDIUM)") ~ "42",
+prod_unit_oth %in%c("TUBE (LARGE)","SATCHET (LARGE)", "SATCHET (150 GRAM)") ~ "43", 
+prod_unit_oth %in%c("CLUSTER") ~ "44", 
+prod_unit_oth == "TABLESPOON"~ "50", 
+prod_unit_oth == "PACKET (SMALL)"~ "54", 
+prod_unit_oth == "PACKETS"~ "60",
+prod_unit_oth %in% c("SATCHET (250 GRAM)","PACKET 250 GRAM") ~  "65", 
+prod_unit_oth == "TIN"~ "72",
+prod_unit_oth == "TIN (500G)"~ "73"))) %>% 
+  filter(prod_unitA==23 & is.na(prod_nsu))  %>% 
+  arrange(desc(kg_d)) %>% 
+  distinct(prod_unit_oth) %>% pull()
+
+
+
+code_list <- vector()
+
+for(i in 1:length(units_na)){
+  
+  code_list[i] <- wb_factor %>% filter(grepl(units_na[i], unit, ignore.case = TRUE)) %>% 
+    distinct(unit_code)
+}
+
+
+# Consumption calculation 
+
+ihs4 <- ihs4 %>% 
+  mutate(prod_nsu = ifelse(prod_unitA!=23, prod_unitA, 
+                           case_when(
+                             prod_unit_oth == "KILOGRAM" ~ "1",
+                             prod_unit_oth == "50 KG" ~ "2",
+                             prod_unit_oth %in% c("PAIL (LARGE)") ~ "4C",
+                             prod_unit_oth %in% c("PAILMEDIUM", "MEDIUMPALL") ~ "4B",
+                             prod_unit_oth %in% c("PAIL (SMALL)", "PAILSMALL") ~ "4A",
+                             prod_unit_oth %in% c("PAIL", "20L PAIL" ,"10LITREPAIL,"  ) ~ "4",
+                             prod_unit_oth == "PAIL (MEDIUM)" ~ "4B",
+                             prod_unit_oth %in% c("NO. 10 PLATE", "PLATE") ~ "6", 
+                             prod_unit_oth == "NO. 12 PLATE" ~ "7", 
+                             prod_unit_oth == "NO. 24 PLATE" ~ "7B", 
+                             prod_unit_oth %in% c("BIGBUNCH") ~ "8C",
+                             prod_unit_oth %in% c("BUNDLE", "BUCH", "BUSH") ~ "8",
+                             prod_unit_oth == "PIECE (SMALL)"~"9A", 
+                             prod_unit_oth %in% c("PIECE MEDIUM","PIECE (MEDIUM)") ~"9B",
+                             prod_unit_oth == "PIECE (LARGE)" ~ "9C", 
+                             prod_unit_oth %in% c("PIECES","PIECE", "MANGO", "PIECE (ONE)", "PEACE", "PIACE", "PEICE", "PIEC3", "MICE" ) ~ "9",
+                             prod_unit_oth %in% c("HEAP", "HEA", "HEP") ~ "10", 
+                             prod_unit_oth %in% c("HEAP", "HEAP (SMALL)","HEAP (MEDIUM)") ~ "10", 
+                             prod_unit_oth == "HEAP (LARGE)" ~ "10C", 
+                             prod_unit_oth == "BOTTLE" & item_code == "803"~ "15",
+                             prod_unit_oth == "CUP" ~ "16",
+                             prod_unit_oth == "GRAMS"~ "18", 
+                             prod_unit_oth %in% c("TEASPOON", "SPOON") ~ "20",
+                             prod_unit_oth %in% c("SATCHET (MEDIUM)") ~ "22B",
+                             prod_unit_oth %in% c("SATCHET (SMALL)") ~ "22B",
+                             prod_unit_oth %in% c("TUBE /SACHET","SATCHET (500 GRAM)", "TUBE") ~ "22",
+                             prod_unit_oth %in% c("TINA(FLAT)", "TINA(SMALL)" ) ~ "25A",
+                             prod_unit_oth %in% c("TINA (HEAPED)", "MEDIUM (PLATE)") ~ "25B",
+                             prod_unit_oth %in% c("5L BUCKET", "5 LITRES", "BUCKET") ~ "26", 
+                             prod_unit_oth %in% c("PACKET 100 GRAM", "PACKET 150 GRAM") ~ "34",
+                             prod_unit_oth == "SATCHET (20 GRAM)"~ "41",
+                             prod_unit_oth %in% c("SATCHET (MEDIUM)", "TUBE (MEDIUM)") ~ "42",
+                             prod_unit_oth %in%c("TUBE (LARGE)","SATCHET (LARGE)", "SATCHET (150 GRAM)") ~ "43", 
+                             prod_unit_oth %in%c("CLUSTER") ~ "44", 
+                             prod_unit_oth == "TABLESPOON"~ "50", 
+                             prod_unit_oth == "PACKET (SMALL)"~ "54", 
+                             prod_unit_oth == "PACKETS"~ "60",
+                             prod_unit_oth %in% c("SATCHET (250 GRAM)","PACKET 250 GRAM") ~  "65", 
+                             prod_unit_oth == "TIN"~ "72",
+                             prod_unit_oth == "TIN (500G)"~ "73"))) 
+
+ihs4 %>% filter(is.na(prod_nsu) & !is.na(prod_quant) & prod_quant >0)
+
+ihs4 %>% 
+  mutate(dummy_prod = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
+                             kg_d, NA)) %>%
+           filter(is.na(dummy_prod) & is.na(prod_quant) & prod_quant ==0)
+
+ihs4  <- ihs4 %>% 
+  mutate(prod_kg_d = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
+                             kg_d, NA)) 
+
+ihs4 %>% filter(item_code == "820" & !is.na(cons_quant))
+
+ihs4 %>% filter(grepl("maize", item, ignore.case = TRUE)) %>% 
+  group_by(item_code, item, region ) %>% 
+  summarise( N = n(),
+            mean = mean(prod_kg_d, na.rm = TRUE), 
+            SD = sd(prod_kg_d, na.rm = TRUE))
+
+2481+4220+5736
+  
 
