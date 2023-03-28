ea4ed389181fecd627d8c0318d2bc89dd53f9682
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-03-28 23:05:58 +0100

    cleaning workflow

--- ihs4_cleaning.R
+++ ihs4_cleaning.R
@@ -10,11 +10,24 @@ ihs4 <- read.csv(here::here("data", "inter-output", "hh_mod_g_processed.csv"))
 wb_factor <- read.csv(here::here('data', "factors", 'wb_conv_factors_mwi.csv'), fileEncoding="UTF-8-BOM")
 nsu.df <- read.csv(here::here('data', "factors", 'nsu.factors.csv'), fileEncoding="UTF-8-BOM")
 
-# 
+
 
 nsu.df <- nsu.df %>% 
-  mutate(cons_unitA = str_extract(measure_id, "(?<=\\_)[:alnum:]+")) %>% 
-  left_join(., ihs4 %>% distinct(cons_unitA, unit))
+  pivot_longer(., 
+               cols = starts_with("ihs4factor"),
+               names_to = "region", 
+               values_to = "ihs4_factor") %>% 
+#  mutate(cons_unitA = str_extract(measure_id, "(?<=\\_)[:alnum:]+")) %>% 
+ separate(measure_id, c("item_code", "cons_unitA"), sep ="_", remove = FALSE) %>% 
+  left_join(., ihs4 %>% distinct(cons_unitA, unit)) 
+
+nsu.df$region[nsu.df$region == "ihs4factor_n"] <- 1
+nsu.df$region[nsu.df$region == "ihs4factor_c"] <- 2
+nsu.df$region<- as.integer(nsu.df$region)
+nsu.df$item_code <- as.integer(nsu.df$item_code)
+
+nsu.df$region[nsu.df$region == "ihs4factor_s"] <- 3
+
 
 #Rename variables
 
@@ -203,20 +216,50 @@ ihs4 %>% filter(is.na(prod_nsu) & !is.na(prod_quant) & prod_quant >0)
 ihs4 %>% 
   mutate(dummy_prod = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
                              kg_d, NA)) %>%
-           filter(is.na(dummy_prod) & is.na(prod_quant) & prod_quant ==0)
+           filter(is.na(dummy_prod) & !is.na(prod_quant)) %>% 
+          filter(prod_quant >0) %>% select(ends_with("quant"), cons_unitA, prod_unitA)
 
+# Using food consumed in kg/d calculated from total consumption (same quantity & same units) to own production
 ihs4  <- ihs4 %>% 
   mutate(prod_kg_d = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
-                             kg_d, NA)) 
+                             kg_d, NA), 
+         comment = ifelse(!is.na(prod_kg_d), "Quantity imputed from variable \"kg_d\" because quantity and units were the same", NA))
+ 
+# Using food consumed in kg/d calculated from total consumption (same quantity, but different units) to own production - Assumption.         
+ihs4  <- ihs4 %>%         
+  mutate(
+    comment = ifelse(is.na(prod_kg_d) & cons_quant == prod_quant,
+        "Quantity imputed from variable \"kg_d\" using only quantity, as units were not the same", comment), 
+    prod_kg_d = ifelse(is.na(prod_kg_d) & cons_quant == prod_quant, 
+                            kg_d, prod_kg_d))
+
+
+# Calculating from factor from NSU from consumed to own production
+
+ihs4  <- ihs4 %>% 
+  mutate(comment = ifelse(is.na(prod_kg_d) & !is.na(prod_quant) & cons_unitA == prod_unitA,
+                          "Quantity calculated from factor (same cons_unitA) different quantity", comment),        
+      prod_kg_d = ifelse(is.na(prod_kg_d) & !is.na(prod_quant) & cons_unitA == prod_unitA,  
+                            (prod_quant*factor)/7, prod_kg_d))
+
+ihs4 %>% 
+  filter(is.na(prod_kg_d) & !is.na(prod_quant)) %>% 
+  filter(prod_quant >0) %>% select(ends_with("quant"), cons_unitA, prod_unitA, item_code_st, region) %>% 
+  left_join(., nsu.df, by = c("item_code_st"= "item_code", "region", "prod_unitA" = "cons_unitA"))
+
+
+ihs4 %>% 
+  filter(is.na(prod_kg_d) & !is.na(prod_quant)) %>% 
+  filter(prod_quant >0) %>% select(ends_with("quant"), cons_unitA, prod_unitA)
 
 ihs4 %>% filter(item_code == "820" & !is.na(cons_quant))
 
 ihs4 %>% filter(grepl("maize", item, ignore.case = TRUE)) %>% 
-  group_by(item_code, item, region ) %>% 
+  group_by(item_code, item, region) %>% 
   summarise( N = n(),
             mean = mean(prod_kg_d, na.rm = TRUE), 
             SD = sd(prod_kg_d, na.rm = TRUE))
 
-2481+4220+5736
+
   
 
