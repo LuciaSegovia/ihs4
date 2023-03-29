e67af09fd1ccceeef20163204c5c68a9f542ca6f
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-03-29 17:58:56 +0100

    cleaned nsu own produciton variable and added documentation

--- ihs4_cleaning.R
+++ ihs4_cleaning.R
@@ -1,16 +1,16 @@
 
-#####------------START HERE: Loading dataset-----------#####
+# Loading dataset -----
 
 library(tidyverse)
 
-#-------Read in the file------#
+# Read in the file------
 
 
 ihs4 <- read.csv(here::here("data", "inter-output", "hh_mod_g_processed.csv"))
 nsu.df <- read.csv(here::here('data', "factors", 'nsu.factors.csv'), fileEncoding="UTF-8-BOM")
 
 
-# Prepare NSU file
+## Prepare NSU file ----
 
 nsu.df <- nsu.df %>% 
   pivot_longer(., 
@@ -28,21 +28,43 @@ nsu.df$region[nsu.df$region == "ihs4factor_s"] <- 3
 nsu.df$region<- as.integer(nsu.df$region)
 nsu.df$item_code <- as.integer(nsu.df$item_code)
 
-# Own-production from total consumption
+# Own-production NSU to SU (kg/d) ----
 
 ihs4 %>% 
-  mutate(dummy_prod = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
-                             kg_d, NA)) %>%
-           filter(is.na(dummy_prod) & !is.na(prod_quant)) %>% 
-          filter(prod_quant >0) %>% select(ends_with("quant"), cons_unitA, prod_unitA)
+  left_join(., nsu.df, by = c("item_code_st"= "item_code", "region",
+                              "prod_unitA" = "cons_unitA")) %>% 
+  filter(is.na(ihs4_factor))
+
+ihs4 <- ihs4 %>% 
+  left_join(., nsu.df, by = c("item_code_st"= "item_code", "region",
+                              "prod_unitA" = "cons_unitA")) %>% 
+  mutate(prod_kg_d = (prod_quant*ihs4_factor)/7) 
+  
+sum(is.na(ihs4$prod_kg_d))
+
+# Own-production from total consumption
+
+ihs4 %>% filter(is.na(prod_kg_d)) %>% 
+  mutate(prod_kg_d = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
+                             kg_d, prod_kg_d)) %>%
+           filter(is.na(prod_kg_d) & !is.na(prod_quant)) %>% 
+          filter(prod_quant >0) %>% dplyr::select(ends_with("quant"), cons_unitA, prod_unitA)
 
 # 1) Using food consumed in kg/d calculated from total consumption (same quantity & same units) to own production
 ihs4  <- ihs4 %>% 
-  mutate(prod_kg_d = ifelse(cons_quant == prod_quant & cons_unitA == prod_unitA, 
-                             kg_d, NA), 
-         comment = ifelse(!is.na(prod_kg_d), "Quantity imputed from variable \"kg_d\" because quantity and units were the same", NA))
+  mutate( comment = ifelse(is.na(ihs4$prod_kg_d) &
+                             cons_quant == prod_quant & 
+                             cons_unitA == prod_unitA,
+                          "Quantity imputed from variable \"kg_d\" because quantity and units were the same", NA), 
+    prod_kg_d = ifelse(is.na(ihs4$prod_kg_d) &
+                          cons_quant == prod_quant & 
+                            cons_unitA == prod_unitA, 
+                             kg_d, prod_kg_d)) 
  
+sum(is.na(ihs4$prod_kg_d))
+
 # 2) Using food consumed in kg/d calculated from total consumption (same quantity, but different units) to own production - Assumption.         
+
 ihs4  <- ihs4 %>%         
   mutate(
     comment = ifelse(is.na(prod_kg_d) & cons_quant == prod_quant,
@@ -68,6 +90,9 @@ ihs4 %>%
   filter(is.na(ihs4_factor)) %>% distinct(prod_unitA, cons_unitA)
 
 ihs4 %>% 
+  filter(is.na(prod_kg_d)  & prod_quant >0) 
+
+ihs4 %>% 
   filter(is.na(prod_kg_d)  & prod_quant >0) %>% 
   filter(grepl("maize", item, ignore.case = TRUE))
 
@@ -83,12 +108,28 @@ ihs4 %>%
 
 ihs4 %>% filter(item_code == "820" & !is.na(cons_quant))
 
-ihs4 %>% filter(grepl("maize", item, ignore.case = TRUE)) %>% 
-  dplyr::group_by(item_code, item, region) %>% 
+variables <- c("item_code", "item", "region")
+variables <- c("item_code", "item", "reside")
+
+ihs4 %>% filter(grepl("maize|fish", item, ignore.case = TRUE)) %>% 
+  dplyr::group_by(across(variables)) %>% 
   dplyr::summarise( 
-            prod_mean= mean(prod_kg_d, na.rm = TRUE), 
-            cons_mean = mean(kg_d, na.rm = TRUE), 
-            SD = sd(prod_kg_d, na.rm = TRUE)) 
+            prod_sum = sum(prod_kg_d, na.rm = TRUE),
+            cons_sum = sum(kg_d, na.rm = TRUE),
+            prod_perc = prod_sum/cons_sum*100
+           # prod_mean= mean(prod_kg_d, na.rm = TRUE), 
+          #  cons_mean = mean(kg_d, na.rm = TRUE), 
+           # SD = sd(prod_kg_d, na.rm = TRUE)
+          ) %>% arrange(desc(cons_sum)) %>% View()
+
 
   
+ihs4 %>% filter(grepl("maize", item, ignore.case = TRUE)) %>% 
+  filter(prod_kg_d <20) %>% 
+  ggplot(aes(as.factor(item), prod_kg_d)) + 
+  geom_boxplot(aes(colour = as.factor(reside)))
 
+ihs4 %>% filter(grepl("fish", item, ignore.case = TRUE)) %>% 
+  filter(prod_kg_d <20) %>% 
+  ggplot(aes(as.factor(item), prod_kg_d)) + 
+  geom_boxplot(aes(colour = as.factor(region)))
